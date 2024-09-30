1bb95e3f130009b6b4843d178e280cc0b24911fa
Erfan <mrenmityerf@gmail.com>
2024-09-30 21:13:19 +0330

    hero insterted | changes on section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <Fragment>
       <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
         <Header></Header>
+        <Hero></Hero>
       </div>
       <ButtonGradient></ButtonGradient>
     </Fragment>
