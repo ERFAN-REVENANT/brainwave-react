40c90cf202196f1e6f10bc788966f142667ed7c2
Erfan <mrenmityerf@gmail.com>
2024-10-02 15:19:45 +0330

    logos insterted and done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,7 @@ import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import Generating from "./Generating";
 import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -88,6 +89,7 @@ const Hero = () => {
           </div>
           <BackgroundCircles></BackgroundCircles>
         </div>
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block"></CompanyLogos>
       </div>
       <BottomLine></BottomLine>
     </Section>
